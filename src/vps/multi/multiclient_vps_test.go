891054bff0713f447ab05a437677a48674b5f5a6
linyujia <1072308682@qq.com>
2019-05-14 14:57:23 +0200

    coap experiments

---
+++ src/vps/multi/multiclient_vps_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+	"os"
+	"log"
+	coap "github.com/lorrin/go-coap"
+)
+
+func BenchmarkVpsMulti(b *testing.B){
+	req := coap.Message{
+			Type:		coap.Confirmable,
+			Code:		coap.GET,
+			MessageID:	uint16(1),
+			Payload:	[]byte("hello"),
+	}
+	path := "my/test"
+	if len(os.Args) >1 {
+			path = os.Args[1]
+	}
+	req.SetPathString(path)
+	for i:=0;i<b.N;i++{
+		c, err := coap.Dial("udp","142.93.161.16:5683")
+		if err != nil{
+			log.Println("err in Dial..")
+		}
+		
+		rv, err := c.Send(req)
+		if err != nil{
+			if rv != nil{
+				if err!= nil{
+					log.Println("err in send..",err)
+				}
+//				payload := string(rv.Payload)
+//				log.Println("Got response message payload:",payload,i)
+			}
+			rv, err = c.Receive()
+		}
+		
+	}
+}
---
+++ src/vps/one/oneclient_vps_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+	"os"
+	"log"
+	coap "github.com/lorrin/go-coap"
+)
+
+func BenchmarkVpsOne(b *testing.B){
+	req := coap.Message{
+			Type:		coap.Confirmable,
+			Code:		coap.GET,
+			MessageID:	uint16(1),
+			Payload:	[]byte("hello"),
+	}
+	path := "my/test"
+	if len(os.Args) >1 {
+			path = os.Args[1]
+	}
+	req.SetPathString(path)
+	
+	c, err := coap.Dial("udp","142.93.161.16:5683")
+	if err != nil{
+		log.Println("err in Dial..")
+	}
+	for i:=0;i<b.N;i++{
+		rv, err := c.Send(req)
+		if err != nil{
+			if rv != nil{
+				if err!= nil{
+					log.Println("err in send..",err)
+				}
+//				payload := string(rv.Payload)
+//				log.Println("Got response message payload:",payload,i)
+			}
+			rv, err = c.Receive()
+		}
+		
+	}
+}
