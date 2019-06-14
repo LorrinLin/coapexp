766d06b00e5a551e5905922d1f00817566893a55
linyujia <1072308682@qq.com>
2019-06-14 13:55:42 +0200

    'xx'

--- src/vps/one/oneclient_vps_test.go
+++ src/vps/one/oneclient_vps_test.go
@@ -25,16 +25,16 @@ func BenchmarkVpsOne(b *testing.B){
 		log.Println("err in Dial..")
 	}
 	for i:=0;i<b.N;i++{
-		rv, err := c.Send(req)
+		c.Send(req)
 		if err != nil{
 			if rv != nil{
 				if err!= nil{
 					log.Println("err in send..",err)
 				}
-//				payload := string(rv.Payload)
-//				log.Println("Got response message payload:",payload,i)
+//				  payload := string(rv.Payload)
+//				  log.Println("Got response message payload:",payload,i)
 			}
-			rv, err = c.Receive()
+			 rv, err = c.Receive()
 		}
 		
 	}
